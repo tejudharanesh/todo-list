47764079e3223fda4ff1834e52ea1c3cd92f4b97
TEJU D <111564230+tejudharanesh@users.noreply.github.com>
2024-04-29 20:29:13 +0530

    Update app.jsx`

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,7 +3,7 @@ import ToDoItem from "./toDoItem";
 import Input from "./input";
 
 function App() {
-    const [ items,setItems] = useState([]); //creating  a state variable for our list(array) of tasks.
+    const [ items,setItems] = useState([]);      //creating  a state variable for our list(array) of tasks.
 
     function addItem(inputText){
         setItems(prevItems => {                  // addItem function  is used to add new task in the array using setItem 
@@ -13,7 +13,7 @@ function App() {
 
     function  deleteItem(itemIndex) {
       setItems( prevItems => {
-        return prevItems.filter(                //deleteItem used to delete the selected task from the array based on the index 
+        return prevItems.filter(                    //deleteItem used to delete the selected task from the array based on the index 
           (items,index) => {                        //retrived from the toDoItem jsx file
             return  index !== itemIndex;
           }
@@ -23,21 +23,21 @@ function App() {
 
 
   return (
-    <div className="container">
-      <div className="heading">
+    <div className = "container">
+      <div className = "heading">
         <h1>To-Do List</h1>
       </div> 
 
-      <Input onAdd = {addItem}/>    {/* calling input component  and passing a callback function */}
+      <Input onAdd = {addItem}/>            {/* calling input component  and passing a callback function */}
     
       <div>
         <ul>
-          {items.map((todoItem,index) =>    //calling ToDoItem component  and passing props in it.
+          {items.map((todoItem,index) =>      //calling ToDoItem component  and passing props in it.
             <ToDoItem 
-            item={todoItem} 
-            key={index} 
-            id={index} 
-            onChecked = {deleteItem}    // passing an event handler onChecked  which is calling deleteItem function
+            item = {todoItem} 
+            key = {index} 
+            id = {index} 
+            onChecked = {deleteItem}          // passing an event handler onChecked  which is calling deleteItem function
             />
             )}
         </ul>
@@ -45,5 +45,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
